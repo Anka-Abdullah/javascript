3700b9ae22cf073c94a6691145d107abb5e1d3dc
Anka-Abdullah <ankaabdullah.id@gmail.com>
2020-11-16 14:46:29 +0700

    task 1

---
+++ palindrome.js
@@ -0,0 +1,16 @@
+function palindrome(text) {
+    text = text.toLowerCase()
+    str = ''
+    array = text.split('')
+    for (i = text.length - 1; i >= 0; i--){
+        str += array[i]
+    }
+    if(str == text) {
+        console.log('Palindrom')
+    }else{
+        console.log('Bukan Palindrom')
+    }
+}
+
+palindrome('Malam')
+palindrome('Ibu')
