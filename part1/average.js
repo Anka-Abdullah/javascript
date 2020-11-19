1bd2df77b405949858b674093de2eb411122fe2a
Anka-Abdullah <ankaabdullah.id@gmail.com>
2020-11-19 09:38:52 +0700

    edit

--- part1/average.js
+++ part1/average.js
@@ -1,21 +1,16 @@
 function rataRataUn(mtk, indo, inggris, ipa) {
-    if (mtk > 100 && indo > 100 && inggris > 100 && ipa > 100){
-        const average = (mtk + indo + inggris + ipa) / 4
-        if (average >= 90) {
-            console.log(`Rata-rata: ${average}\nGrade : A`)
-        } else if (average >= 80) {
-            console.log(`Rata-rata: ${average}\nGrade : B`)
-        } else if (average >= 70) {
-            console.log(`Rata-rata: ${average}\nGrade : C`)
-        } else if (average >= 60) {
-            console.log(`Rata-rata: ${average}\nGrade : D`)
-        } else {
-            console.log(`Rata-rata: ${average}\nGrade : E`)
-        }
+    const average = (mtk + indo + inggris + ipa) / 4
+    if (average >= 90) {
+        console.log(`Rata-rata: ${average}\nGrade : A`)
+    } else if (average >= 80) {
+        console.log(`Rata-rata: ${average}\nGrade : B`)
+    } else if (average >= 70) {
+        console.log(`Rata-rata: ${average}\nGrade : C`)
+    } else if (average >= 60) {
+        console.log(`Rata-rata: ${average}\nGrade : D`)
     } else {
-        console.log('input nilai dengan benar!')
+        console.log(`Rata-rata: ${average}\nGrade : E`)
     }
-    
 }
 
 const matematika = 80
