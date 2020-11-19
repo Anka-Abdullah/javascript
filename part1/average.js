93fecf160bc512ab059cc85f9e2f36f988510b20
Anka-Abdullah <ankaabdullah.id@gmail.com>
2020-11-19 09:16:16 +0700

    edit average

--- part1/average.js
+++ part1/average.js
@@ -1,20 +1,15 @@
 function rataRataUn(mtk, indo, inggris, ipa) {
     const average = (mtk + indo + inggris + ipa) / 4
     if (average >= 90) {
-        console.log(`Rata-rata: ${average}`)
-        console.log('Grade : A')
+        console.log(`Rata-rata: ${average}\nGrade : A`)
     } else if (average >= 80) {
-        console.log(`Rata-rata: ${average}`)
-        console.log('Grade : B')
+        console.log(`Rata-rata: ${average}\nGrade : B`)
     } else if (average >= 70) {
-        console.log(`Rata-rata: ${average}`)
-        console.log('Grade : C')
+        console.log(`Rata-rata: ${average}\nGrade : C`)
     } else if (average >= 60) {
-        console.log(`Rata-rata: ${average}`)
-        console.log('Grade : D')
+        console.log(`Rata-rata: ${average}\nGrade : D`)
     } else {
-        console.log(`Rata-rata: ${average}`)
-        console.log('Grade : E')
+        console.log(`Rata-rata: ${average}\nGrade : E`)
     }
 }
 
