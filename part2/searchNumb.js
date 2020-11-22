7a4790a522f183802d053dda541d4d286dbbce38
Anka-Abdullah <ankaabdullah.id@gmail.com>
2020-11-22 22:25:13 +0700

    edit

--- part2/searchNumb.js
+++ part2/searchNumb.js
@@ -4,14 +4,19 @@ function searchNumb(first, last, array){
     } else if (array.length <= 5){
         console.log('data dalam Array harus lebih dari 5')
     }else{
-        str= ''
+        str= []
         for (i = 0; i <= array.length-1; i++){
             if (array[i] > first && array[i] < last) {
-                str += array[i] + ' '
+                str.push(array[i])
             }
         }
-        console.log(str)
+        if(str.length < 1){
+            console.log('angka yang dicari tidak ada')
+        }else{
+            console.log(str)
+        }
     }
     
 }
 searchNumb(5, 20, [2, 25, 4, 14, 17, 30, 8])
+// searchNumb(18, 20, [2, 16, 4, 14, 17, 13, 8])
